eac632d8df351e140f78f4ef72ef49455528571f
jeninidi <jeninidi@abv.bg>
2016-10-05 12:46:58 +0200

    Add project files.

---
+++ PinCodeInterface/Model/Event.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
+
+namespace PinCodeInterface.Model
+{
+    class Event
+    {
+        public string _title;
+        public string _description;
+        public string _place;
+        public DateTime _dateTime;
+        
+
+        public string Title { get;  set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public Event() 
+        {
+            
+        }
+
+        public Event(string _title, string _description, string _place, DateTime _dateTime)
+
+        {
+            Title = _title;
+            Description = _description;
+            Place = _place;
+            DateTime = _dateTime;
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Title: {0}, Description: {1}, Place: {2}, DateTime: {3}", Title, Description, Place, DateTime);
+        }
+
+    }
+
+
+
+}
